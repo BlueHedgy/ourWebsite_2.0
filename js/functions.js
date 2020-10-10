327165cabf88be43e5f354491d5a2c763791e868
Blue <blueaflmao@outlook.com>
2020-10-10 16:03:31 +0300

    implement linkToArt and linkToHome, turn those naviButtons to hyperLink

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,28 @@
+function pageInit()
+{
+    artButtonClick();
+    homeButtonClick();
+}
+
 function artButtonClick()
 {
     var linkToArt = document.createElement("a");
+    linkToArt.setAttribute("class", "naviLink");
     linkToArt.href = "Artwork.html";
     linkToArt.innerHTML = "ARTWORKS";
+
     var artButton = document.getElementsByClassName("naviMenu")[4];
     artButton.appendChild(linkToArt);
 }
 
+
+function homeButtonClick()
+{
+    var linkToHome = document.createElement("a");
+    linkToHome.setAttribute("class", "naviLink");
+    linkToHome.href = "OurWebsiteV2.html";
+    linkToHome.innerHTML = "MAIN PAGE";
+
+    var homeButton = document.getElementsByClassName("naviMenu")[0];
+    homeButton.appendChild(linkToHome);
+}
